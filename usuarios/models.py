025d61c5e27a85d2bbd4c81856b1ed7aa68f52bb
gabgom2 <236346865+gabgom2@users.noreply.github.com>
2025-10-17 07:27:13 -0300

    v 0.1d. Agregada clase estudiantes a modelos y migrada

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Estudiante(models.Model):
+    
+    class Nivel(models.TextChoices):
+        inicial = "inicial"
+        intermedio = "intermedio"
+        avanzado = "avanzado"
+        
+        
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    dni = models.IntegerField(unique=True)
+    telefono = models.IntegerField(unique=True)
+    email = models.EmailField()
+    barrio_residencia = models.CharField(max_length=100)
+    nivel = models.CharField(choices=Nivel.choices)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Nombre completo: {self.apellido}, {self.nombre} / DNI: {self.dni} / Nivel: {self.nivel}"
+    
